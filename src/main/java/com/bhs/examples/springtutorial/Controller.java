913cb9a52b900629319cc03e5e4cd5d6bc888915
Hema Sai Bonda <hemasaibonda1@gmail.com>
2024-09-02 11:56:03 +0530

    Todo Controller initial update with get and post mapping

--- src/main/java/com/bhs/examples/springtutorial/Controller.java
+++ src/main/java/com/bhs/examples/springtutorial/Controller.java
@@ -3,19 +3,46 @@ package com.bhs.examples.springtutorial;
 import okhttp3.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+
+//@RestController
+//public class Controller {
+//    @GetMapping("/todo/{id}")
+//    public String getTodoById(@PathVariable("id") String id) throws IOException{
+//        OkHttpClient client = new OkHttpClient();
+//        Request request = new Request.Builder().url("https://jsonplaceholder.typicode.com/todos/"+id).build();
+//        try(Response response = client.newCall(request).execute()){
+//            return response.body().string();
+//        }
+//    }
+//}
 
 @RestController
-public class Controller {
-    @GetMapping("/todo/{id}")
-    public String getTodoById(@PathVariable("id") String id) throws IOException{
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url("https://jsonplaceholder.typicode.com/todos/"+id).build();
-        try(Response response = client.newCall(request).execute()){
-            return response.body().string();
+public class Controller{
+    List<Todo> todoList;
+    public Controller(){
+        todoList = new ArrayList<Todo>();
+        todoList.add(new Todo(1,1,"Learning Spring",false));
+        todoList.add(new Todo(2,1,"Learning React",true));
+    }
+    @GetMapping("/todos")
+    public List<Todo> getTodoList(){
+        return todoList;
+    }
+    @GetMapping("/todos/{id}")
+    public Todo getTodoById(@PathVariable("id") int id){
+        for(Todo t : todoList){
+            if(t.getId() == id){return t;}
         }
+        return null;
+    }
+    @PostMapping("/todos")
+    public Todo addNewTodo(@RequestBody Todo newTodo){
+        todoList.add(newTodo);
+        return newTodo;
     }
 }
---
+++ src/main/java/com/bhs/examples/springtutorial/Todo.java
@@ -0,0 +1,54 @@
+package com.bhs.examples.springtutorial;
+
+public class Todo {
+    private int id;
+    private int userId;
+    private String title;
+    private boolean completed;
+    public Todo(int id,int userId,String title,boolean completed){
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+        this.userId = userId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String toString(){
+        return "Todo{" +
+                "id = " + id + '\'' +
+                ",completed = " + completed + '\'' +
+                ",title = " + title + '\'' +
+                ",userId = " + userId + '\'' +
+                "}";
+    }
+}
